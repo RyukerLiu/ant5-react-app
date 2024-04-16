94021042adfedd160a5f9d5b253626577257505a
Allen Liu <alex03108861@gmail.com>
2024-04-16 11:12:33 +0800

    Fix Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders button with text "Button"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/Button/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+
+test('renders button with primary type', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button');
+  expect(buttonElement).toHaveClass('ant-btn-primary');
 });
